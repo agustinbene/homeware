01fb74d83781bb63e29bf4b245893cee509cc310
jpmartin <46228295+jpmartin-gicom@users.noreply.github.com>
2020-02-13 17:57:12 -0300

    Update homeware.py

--- homeware.py
+++ homeware.py
@@ -554,7 +554,8 @@ def token():
         return response
 
 #Google's endpoint
-#@app.route("/smarthome/", methods=['POST'])
+@app.route("/smarthome", methods=['GET'])
+@app.route("/smarthome/", methods=['POST'])
 @app.route("/smarthome", methods=['POST'])
 def smarthome():
     #Get all data
